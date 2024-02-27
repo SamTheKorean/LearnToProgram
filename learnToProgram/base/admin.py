5fa54903765e47ead7468c161e04583629ed466b
Sam2022moon <104721736+Sam2022moon@users.noreply.github.com>
2024-02-28 03:21:52 +0900

    implemented structures of the project

--- learnToProgram/base/admin.py
+++ learnToProgram/base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Room, Topic, Message
+
+admin.site.register(Topic)
+admin.site.register(Message)
+admin.site.register(Room)
 # Register your models here.
